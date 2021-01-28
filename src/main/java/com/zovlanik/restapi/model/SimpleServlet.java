c0c9fa4a660d7ae370b92f76cc961fe59ee2605a
Zovlanik <Dkflbvbh162>
2021-01-28 12:50:11 +0300

    first commit

---
+++ src/main/java/com/zovlanik/restapi/model/SimpleServlet.java
@@ -0,0 +1,30 @@
+package com.zovlanik.restapi.model;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        message = "I can write here something, but only in english :(";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter messageWriter = response.getWriter();
+        messageWriter.println("<h1>" + message + "<h1>");
+    }
+
+    public void destroy() {
+
+    }
+}
